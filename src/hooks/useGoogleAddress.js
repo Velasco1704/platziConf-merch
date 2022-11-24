5d8ea5add803247176b9a3502c43e55405a4ac58
Daniel Velasco <danilovelga4747@gmail.com>
2022-11-24 15:31:02 -0500

    formateando algunos proyectos

--- src/hooks/useGoogleAddress.js
+++ src/hooks/useGoogleAddress.js
@@ -4,17 +4,13 @@ import axios from "axios";
 export const useGoogleAddress = (address) => {
   const [map, setMap] = useState({});
   const API = `http://api.positionstack.com/v1/forward?access_key=c46c171ff956053ec24ed860ec9236d5&query=${address}`;
+  // const API = `http://api.positionstack.com/v1/forward?access_key=c46c171ff956053ec24ed860ec9236d5&query=${country}, ${city}, ${address}`;
   useEffect(() => {
     const fetchData = async () =>{
       const res = await axios(API)
       setMap(res.data.data[0]);
-      // setMap({
-      //   lag: res.data.data[0].latitude,
-      //   lng: res.data.data[0].longitude,
-      // })
     }
     fetchData();
-    // const res = await axios(API);
   }, []);
   console.log(map)
   return map;
